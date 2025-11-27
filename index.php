2f08836bee9e8caaf77f5ce7679cd1eb847c5d92
Tawheedyahya <tawheedyahya0@gmail.com>
2025-11-27 18:31:57 +0000

    revert after

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-echo 'hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii';
+echo 'hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii';
 ?>
